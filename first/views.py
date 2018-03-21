3faa3f768888a055eabfb7a980e5ebb69136ed1d
Yuta Ogawa <yogawa0417@gmail.com>
2018-03-21 21:40:28 +0900

    Fix an order of posts.

--- first/views.py
+++ first/views.py
@@ -6,5 +6,5 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())\
-                        .order_by('published_date')
+                        .order_by('-published_date')
     return render(request, 'first/post_list.html', {'posts': posts})
