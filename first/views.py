6050b6d46906b9707d2f6daabe297015dd048580
Yuta Ogawa <yogawa0417@gmail.com>
2018-03-21 19:58:42 +0900

    Fix a URL.

--- first/views.py
+++ first/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'first\post_list.html', {})
+    return render(request, 'first/post_list.html', {})
