d26a2d309bbfe33bd8f7aa33a4edbcec4188f2c9
Yuta Ogawa <yogawa0417@gmail.com>
2018-04-02 21:08:55 +0900

    during a implementation.

--- first/views.py
+++ first/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -5,6 +6,10 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())\
+    all_post = Post.objects.filter(published_date__lte=timezone.now())\
                         .order_by('-published_date')
+    paginator = Paginator(all_post, 3)
+
+    page = request.GET.get('p')
+    posts = paginator._get_page(page)
     return render(request, 'first/post_list.html', {'posts': posts})
